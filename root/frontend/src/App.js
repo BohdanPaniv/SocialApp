227e36bb55567c48df5f46a2da7ade07c4091607
BohdanPaniv <bohdan.paniv@gmail.com>
2021-06-23 00:08:44 +0300

    router added

--- root/frontend/src/App.js
+++ root/frontend/src/App.js
@@ -1,6 +1,23 @@
-function App() {
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
+import Authentication from "./pages/authentication/Authentication";
+import ChangePassword from "./pages/changePassword/ChangePassword";
+
+const App = () => {
   return (
-    <div></div>
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <Authentication></Authentication>
+        </Route>
+        <Route path="/changePassword">
+          <ChangePassword></ChangePassword>
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
