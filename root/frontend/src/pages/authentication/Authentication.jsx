e55d06fe25e292d4e75ddad2b5d65e452a49f350
BohdanPaniv <bohdan.paniv@gmail.com>
2021-06-23 00:10:38 +0300

    authenticartion added

---
+++ root/frontend/src/pages/authentication/Authentication.jsx
@@ -0,0 +1,38 @@
+import "./authentication.css";
+import { useState } from "react";
+import LoginCard from "../../components/loginCard/LoginCard";
+import RegisterCard from "../../components/registerCard/RegisterCard";
+
+const Authentication = () => {
+
+  const [isLogin, setIsLogin] = useState(true);
+
+  return (
+    <div className="auth">
+      <div className="auth-container">
+        <div className="auth-left">
+          <h1 className="auth-logo">
+            SocialApp
+          </h1>
+          <span className="auth-desc">
+            Connect to the world, connect to SocialApp.
+          </span>
+        </div>
+        <div className="auth-right">
+          {
+            isLogin ? 
+              <LoginCard
+                isLogin={ isLogin }
+                setIsLogin={ setIsLogin }/> 
+              : 
+              <RegisterCard
+                isLogin={ isLogin }
+                setIsLogin={ setIsLogin }/>
+          }
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Authentication;
