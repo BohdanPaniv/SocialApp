cd74c8d9f629c1468e2abe6b355c7dfb26fad9af
BohdanPaniv <bohdan.paniv@gmail.com>
2021-06-23 00:11:26 +0300

    changePassword page added

---
+++ root/frontend/src/pages/changePassword/ChangePassword.jsx
@@ -0,0 +1,15 @@
+import "./changePassword.css";
+import ChangePasswordCard from "../../components/changePasswordCard/ChangePasswordCard";
+
+const ChangePassword = () => {
+  return (
+    <div className="change-password">
+      <h1 className="change-passwor-logo">
+        SocialApp
+      </h1>
+      <ChangePasswordCard/>
+    </div>
+  );
+}
+
+export default ChangePassword;
