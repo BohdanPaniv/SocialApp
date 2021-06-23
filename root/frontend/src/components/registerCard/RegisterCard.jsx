c1ebdbd8de61a12bc42ac1feaee42d6c8c1d9133
BohdanPaniv <bohdan.paniv@gmail.com>
2021-06-23 23:26:44 +0300

    RegisterCard updated

--- root/frontend/src/components/registerCard/RegisterCard.jsx
+++ root/frontend/src/components/registerCard/RegisterCard.jsx
@@ -1,6 +1,7 @@
-import "./registerCard.css";
+import "./registerCard.scss";
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
+import Card from "../card/Card";
 
 const RegisterCard = ({ isLogin, setIsLogin }) => {
 
@@ -9,55 +10,65 @@ const RegisterCard = ({ isLogin, setIsLogin }) => {
   }
 
   return (
-    <form className="register-form" noValidate>
-      <div className="form-control-register">
-        <TextField 
-          className="form-input-register"
-          label="Name"
-          variant="outlined"/>
-      </div>
-      <div className="form-control-register">
-        <TextField 
-          className="form-input-register"
-          label="Surname"
-          variant="outlined"/>
-      </div>
-      <div className="form-control-register">
-        <TextField 
-          type="email"
-          className="form-input-register"
-          label="Email"
-          variant="outlined"/>
-      </div>
-      <div className="form-control-register">
-        <TextField 
-          type="password"
-          autoComplete="on"
-          className="form-input-register"
-          label="Password" 
-          variant="outlined" />
-      </div>
-      <div className="form-action-register">
-        <Button
-          variant="contained" 
-          className="create-acount-button"
-          color="primary">
-            Sign Up
-        </Button>
-      </div>
-      <div className="form-text-register">
-        <span>Already have Account?</span>
-      </div>
-      <div className="form-action-register last-element">
-        <Button 
-          variant="contained" 
-          className="log-into-button"
-          onClick={ changeIsLogin }
-          color="primary">
-            Log into Account
-        </Button>
-      </div>      
-    </form>
+    <div className="register-card">
+      <Card>
+        <form className="register-form" noValidate>
+          <div className="form-control">
+            <TextField 
+              className="form-input"
+              label="Name"
+              variant="outlined"
+            />
+          </div>
+          <div className="form-control">
+            <TextField 
+              className="form-input"
+              label="Surname"
+              variant="outlined"
+            />
+          </div>
+          <div className="form-control">
+            <TextField 
+              type="email"
+              className="form-input"
+              label="Email"
+              variant="outlined"
+            />
+          </div>
+          <div className="form-control">
+            <TextField 
+              type="password"
+              autoComplete="on"
+              className="form-input"
+              label="Password" 
+              variant="outlined"
+            />
+          </div>
+          <div className="form-action">
+            <Button
+              variant="contained" 
+              className="create-acount-button"
+              color="primary"
+            >
+              Sign Up
+            </Button>
+          </div>
+          <div className="form-text">
+            <span>Already have Account?</span>
+          </div>
+          <div className="form-action last-element">
+            <Button 
+              variant="contained" 
+              className="log-into-button"
+              onClick={ changeIsLogin }
+              color="primary"
+            >
+              Log into Account
+            </Button>
+          </div>      
+        </form>
+      </Card>
+    </div>
   );
 }
 
