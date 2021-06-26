fe09f7b68eea42df4efa2ae52f3e7bc54d94f418
BohdanPaniv <bohdan.paniv@gmail.com>
2021-06-27 00:07:30 +0300

    redux added

--- root/frontend/src/index.js
+++ root/frontend/src/index.js
@@ -1,10 +1,14 @@
-import ReactDOM from 'react-dom';
-import App from './App';
-import { SnackbarProvider } from 'notistack';
+import ReactDOM from "react-dom";
+import App from "./App";
+import { SnackbarProvider } from "notistack";
+import { Provider } from "react-redux";
+import store from "./store/store";
 
 ReactDOM.render(
-  <SnackbarProvider maxSnack={4}>
-    <App />
-  </SnackbarProvider>,
+  <Provider store={store}>
+    <SnackbarProvider maxSnack={4}>
+      <App />
+    </SnackbarProvider>
+  </Provider>,
   document.getElementById('root')
 );
