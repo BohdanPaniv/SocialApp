92c9c5ad717b61f15591579f6413bdadcf072172
BohdanPaniv <bohdan.paniv@gmail.com>
2021-06-27 00:13:57 +0300

    useMessage changed

--- root/frontend/src/hooks/useMessage.js
+++ root/frontend/src/hooks/useMessage.js
@@ -2,22 +2,53 @@ import { useCallback } from "react";
 import { useSnackbar } from 'notistack';
 
 export const useMessage = () => {
-
   const { enqueueSnackbar } = useSnackbar();
 
-  return useCallback(errors => {
+  const showMessage = useCallback((message, type, time = 3000) => {
+    if (!message || !type) {
+      return console.log({ error: "Message or type are empty"});
+    }
 
-    let temp = "";
-
-    errors.forEach(element => {
-      if (element.param !== temp) {
-        temp = element.param;
-
-        enqueueSnackbar(element.msg, {
-          variant: "error",
-          autoHideDuration: 3000 
-        });
-      }
+    enqueueSnackbar(message, {
+      variant: type,
+      autoHideDuration: time 
     });
-  }, [enqueueSnackbar])
-}
+  }, [enqueueSnackbar]);
+
+  return useCallback(data => {
+    if (!data) {
+      return console.log({ error: "Data is empty"});
+    }
+    
+    const error = "error";
+    const success = "success";
+    
+    switch (data.status) {
+      case 400:
+        if (Array.isArray(data.message)) {
+          let temp = "";
+
+          data.message.forEach(element => {
+            if (element.param !== temp) {
+              temp = element.param;
+      
+              showMessage(element.msg, error);
+            }
+          });
+
+          return;
+        }
+        
+        showMessage(data.message, error);
+        break;
+      case 200:
+        if (data?.message) {
+          showMessage(data.message, success);
+        }
+        break;
+      default:
+        showMessage(data.message, error);
+        break;
+    }
+  }, [showMessage]);
+};
