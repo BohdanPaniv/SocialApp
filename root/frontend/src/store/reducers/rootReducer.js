411a27921d040765522143d26e579588d12d6a1c
BohdanPaniv <bohdan.paniv@gmail.com>
2021-07-02 12:27:34 +0300

    rootReducer updated

--- root/frontend/src/store/reducers/rootReducer.js
+++ root/frontend/src/store/reducers/rootReducer.js
@@ -1,8 +1,12 @@
 import { combineReducers } from "redux";
 import authReducer from "./authReducer";
+import userPostsReducer from "./userPostsReducer";
 import responseReducer from "./responseReducer";
+import feedReducer from "./feedReducer";
 
 export const rootReducer = combineReducers({
   auth: authReducer,
-  response: responseReducer
+  response: responseReducer,
+  userPosts: userPostsReducer,
+  feed: feedReducer
 });
