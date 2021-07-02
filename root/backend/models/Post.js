fc40c1ad80b74d26fdb5534bbd5d2ab7ca4dbb0d
BohdanPaniv <bohdan.paniv@gmail.com>
2021-07-02 12:16:24 +0300

    post added

---
+++ root/backend/models/Post.js
@@ -0,0 +1,28 @@
+const { Schema, model } = require("mongoose");
+
+const postSchema = new Schema(
+  {
+    userId: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String
+    },
+    imageName: {
+      type: String
+    },
+    likes: {
+      type: Array,
+      default: [],
+    },
+    createdAt: {
+      type: Number
+    },
+    updatedAt: {
+      type: Number
+    }
+  }
+);
+
+module.exports = model("Post", postSchema);
