31bf214ca0d71a2ea6120393e538c513475efd35
BohdanPaniv <bohdan.paniv@gmail.com>
2021-07-02 12:21:26 +0300

    check auth added

--- root/frontend/src/App.js
+++ root/frontend/src/App.js
@@ -2,23 +2,60 @@ import {
   BrowserRouter as Router,
   Switch,
   Route,
+  Redirect
 } from "react-router-dom";
 import Authentication from "./pages/authentication/Authentication";
 import ChangePassword from "./pages/changePassword/ChangePassword";
+import Home from "./pages/home/Home";
+import { useEffect } from "react";
+import { loadUser } from "./store/actions/authActions";
+import { useDispatch, useSelector } from "react-redux";
+import Profile from "./pages/profile/Profile";
 
 const App = () => {
+  const dispatch = useDispatch();
+  const auth = useSelector(state => state.auth);
+
+  useEffect(() => {
+    dispatch(loadUser());
+  }, [dispatch]);
+
   return (
     <Router>
-      <Switch>
-        <Route exact path="/">
-          <Authentication></Authentication>
-        </Route>
-        <Route path="/changePassword">
-          <ChangePassword></ChangePassword>
-        </Route>
-      </Switch>
+      {
+        !auth.isLoading &&
+        (
+          auth.token ? 
+          (
+            <Switch>
+              <Route exact path="/">
+                <Home/>
+              </Route>
+              <Route path="/changePassword">
+                <ChangePassword/>
+              </Route>
+              <Route path="/profile/:id">
+                <Profile />
+              </Route>
+              <Redirect to="/"/>
+            </Switch>
+          )
+          :
+          (
+            <Switch>
+              <Route exact path="/">
+                <Authentication/>
+              </Route>
+              <Route path="/changePassword">
+                <ChangePassword/>
+              </Route>
+              <Redirect to="/"/>
+          </Switch>
+          )
+        )
+      }
     </Router>
   );
-}
+};
 
 export default App;
